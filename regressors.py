e8737604a89d07bf7f3d6ddd12f33355a1c005dc
klas9802 <klas9806@gmail.com>
2021-11-02 11:37:47 +0800

    V1.2

--- regressors.py
+++ regressors.py
@@ -23,3 +23,23 @@ es = xgboost.callback.EarlyStopping(
     data_name="validation_0",
 )
 
+
+clf = xgboost.XGBRegressor()
+rig = Ridge(alpha=2.0)
+bay = linear_model.BayesianRidge()
+
+clf.fit(X_train, y_train, eval_set=[(X_train, y_train)], callbacks=[es])
+rig.fit(X, y)
+bay.fit(X, y)
+
+# 回歸方法 X_test y_test X_train y_train
+def validation(*args):
+    predict = args[1].predict(args[2])
+    accuracy = explained_variance_score(args[3], predict, multioutput='uniform_average')
+    R2 = rig.score(args[4], args[5])
+    mse = metrics.mean_squared_error(args[3], predict)
+    print("{}_validation : Acc : {:.2f}, R2 : {:.5f}, Mse: {:.2f}".format(args[0], accuracy, R2, mse))
+
+print(validation("Xgboost", clf, X_test ,y_test ,X_train ,y_train))
+print(validation("Ridge", rig, X_test ,y_test ,X_train ,y_train))
+print(validation("Bayesian", bay, X_test ,y_test ,X_train ,y_train))
