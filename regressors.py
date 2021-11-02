f81bb8cc337a261de2d7b5112fdf82122ba019a5
klas9802 <klas9806@gmail.com>
2021-11-02 10:55:46 +0800

    V1.1

--- regressors.py
+++ regressors.py
@@ -7,3 +7,19 @@ from sklearn.metrics import explained_variance_score
 from sklearn.linear_model import Ridge
 from sklearn import linear_model
 from sklearn import metrics
+
+df = pd.read_csv('talibsII.csv', index_col=0)
+
+X = df.drop(['Price'], axis = 1)
+y = df['Price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, shuffle=False)
+
+#XGB parameter
+es = xgboost.callback.EarlyStopping(
+    rounds=100,
+    save_best=True,
+    maximize=False,
+    data_name="validation_0",
+)
+
