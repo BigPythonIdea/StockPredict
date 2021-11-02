60bb732b7550a60bcf74d23914900710ce3c0df0
klas9802 <klas9806@gmail.com>
2021-11-02 10:45:16 +0800

    drop old new respy

---
+++ regressors.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import xgboost
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import explained_variance_score
+from sklearn.linear_model import Ridge
+from sklearn import linear_model
+from sklearn import metrics
